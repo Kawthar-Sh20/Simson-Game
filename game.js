c83c6731b40eeec709318d62c437f0fe80ab18e4
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-06-30 00:13:17 +0300

    Adding nextSequence function

--- game.js
+++ game.js
@@ -1,15 +1,29 @@
-const buttonColors = ["red", "blue", "green", "yellow"]
+
+const buttonColors = ["green", "red", "yellow", "blue"];
 let gamePattern = [];
-let userClickedPattern = []
-let started = false
-let level = 0
+let userClickedPattern = [];
+let started = false;
+let level = 0;
 
 document.addEventListener('keypress', () => {
-
     if (!started) {
-        document.querySelector("#level-title").textContent = `Level ${level}`
+        document.querySelector("#level-title").textContent = `Level ${level}`;
         nextSequence();
-        started = true
+        started = true;
     }
-})
+});
 
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    document.querySelector("#level-title").textContent = `Level ${level}`;
+
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    
+    flashButton(randomChosenColor);
+
+    playSound(randomChosenColor);
+}
